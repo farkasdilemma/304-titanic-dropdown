6f2bd0e1edb48539ac59819b53bae7d4aa7fc55e
farkasdilemma <62410633+farkasdilemma@users.noreply.github.com>
2022-03-23 15:40:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,19 @@ import plotly.graph_objs as go
 
 
 ###### Define your variables #####
-tabtitle = 'Titanic!'
+tabtitle = 'NBA!'
 color1='#92A5E8'
 color2='#8E44AD'
 color3='#FFC300'
-sourceurl = 'https://www.kaggle.com/c/titanic'
-githublink = 'https://github.com/plotly-dash-apps/304-titanic-dropdown'
+sourceurl = 'https://git.generalassemb.ly/intuit-ds-15/05-cleaning-combining-data/blob/master/data/NBA_players_2015.csv'
+githublink = 'https://github.com/farkasdilemma/304-titanic-dropdown/blob/main/app.py'
 
 
 ###### Import a dataframe #######
-df = pd.read_csv("https://raw.githubusercontent.com/austinlasseter/plotly_dash_tutorial/master/00%20resources/titanic.csv")
-df['Female']=df['Sex'].map({'male':0, 'female':1})
-df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
-variables_list=['Survived', 'Female', 'Fare', 'Age']
+df = pd.read_csv("https://raw.git.generalassemb.ly/intuit-ds-15/05-cleaning-combining-data/master/data/NBA_players_2015.csv?token=AAAKJEDTOCI6NUD6ZELUHK3CIPLCE")
+#df['Female']=df['Sex'].map({'male':0, 'female':1})
+#df['Cabin Class'] = df['Pclass'].map({1:'first', 2: 'second', 3:'third'})
+variables_list=[df['bref_team_id'].unique()]
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -31,7 +31,7 @@ app.title=tabtitle
 
 ####### Layout of the app ########
 app.layout = html.Div([
-    html.H3('Choose a continuous variable for summary statistics:'),
+    html.H3('Choose an NBA team to see their top players by points in 2015:'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in variables_list],
@@ -47,34 +47,37 @@ app.layout = html.Div([
 
 ######### Interactive callbacks go here #########
 @app.callback(Output('display-value', 'figure'),
-              [Input('dropdown', 'value')])
-def display_value(continuous_var):
-    grouped_mean=df.groupby(['Cabin Class', 'Embarked'])[continuous_var].mean()
-    results=pd.DataFrame(grouped_mean)
+              [Input('dropdown', 'team_var')])
+def display_value(team_var):
+    
+    top_players = nba.sort_values(['bref_team_id','pts'],ascending = False).groupby('bref_team_id')[['player','bref_team_id','PER','VORP','WS']].head(3)
+
+
+    results=pd.DataFrame(top_players)
     # Create a grouped bar chart
     mydata1 = go.Bar(
-        x=results.loc['first'].index,
-        y=results.loc['first'][continuous_var],
-        name='First Class',
+        x=results['PER'].index,
+        y=results[results['bref_team_id']==team_var]['PER'],
+        name='PER',
         marker=dict(color=color1)
     )
     mydata2 = go.Bar(
-        x=results.loc['second'].index,
-        y=results.loc['second'][continuous_var],
-        name='Second Class',
+        x=results.loc['WS'].index,
+        y=results[results['bref_team_id']==team_var]['WS'],
+        name='Win Shares',
         marker=dict(color=color2)
     )
     mydata3 = go.Bar(
-        x=results.loc['third'].index,
-        y=results.loc['third'][continuous_var],
-        name='Third Class',
+        x=results.loc['VORP'].index,
+        y=results[results['bref_team_id']==team_var]['VORP'],
+        name='Value Over Replacement Player',
         marker=dict(color=color3)
     )
 
     mylayout = go.Layout(
         title='Grouped bar chart',
-        xaxis = dict(title = 'Port of Embarkation'), # x-axis label
-        yaxis = dict(title = str(continuous_var)), # y-axis label
+        xaxis = dict(title = 'Players'), # x-axis label
+        yaxis = dict(title = str(team_var)), # y-axis label
 
     )
     fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
